8445aab7720dd66a4ab3663e56fdd9f293481321
chayensheel <chayensheel403@gmail.com>
2023-01-14 12:57:38 +0600

    class two

---
+++ script.js
@@ -0,0 +1,112 @@
+/*let fristName ="asif";
+let age = 33;
+console.log(fristName);
+console.log(age);
+*/
+
+//Number
+/*let age =22;
+console.log(age);
+//string
+let fristName = "asif";
+console.log(fristName);
+//Boolean
+let isSinger = true;
+console.log(isSinger);
+//Undefined
+let homeCity;
+console.log(homeCity);
+//Null
+let income = null;
+console.log(income);
+/*console.log(typeof age);
+console.log(typeof homeCity);
+console.log(typeof isSinger);
+*/
+/*homeCity = "Dhaka"
+console.log(isSinger)
+console.log(homeCity);
+isSinger = "Bangladshi Singer";
+console.log(isSinger);
+console.log(typeof income);  javascript Bug
+*/
+
+//let fristName ="asif";
+
+/*
+1.let
+2.const
+3.var
+*/
+
+//let
+/*let myAge = 33;
+console.log(myAge);
+myAge =35;
+console.log(myAge);
+//const
+const birthYear =1996;
+console.log(birthYear);
+//var
+var job ="Teacher"
+console.log(job);
+*/
+//Javascript oprator
+/*console.log(18-12);
+let profit= 30-28;
+console.log(profit);
+*/
+// let currentYear =2023;
+// let myAge = currentYear-1996;
+// console.log(myAge);
+// let joyage = currentYear-2010;
+// console.log(joyage);
+// console.log(myAge,joyage);
+
+// let ageDiffer = myAge-joyage;
+// console.log(ageDiffer);
+// console.log(10+9);
+// console.log(20-8);
+// console.log(5*5);
+// console.log(30/5);
+// console.log(3**3);
+// console.log(25%3);
+
+// let x = 10;
+// x++;
+// console.log(x);
+
+// let y =20;
+// y--;
+// console.log(y);
+
+// console.log(100 - 5 + 40 * 20 /2 )
+
+// let fristName ="Asif";
+// let lastName ="Akbar";
+// console.log(fristName + " " + lastName);
+
+// =
+// +=
+// -=
+// *=
+// /=
+
+let item = 10 + 4;
+console.log(item);
+
+let x = 15;
+x += 10;
+console.log(x);
+
+let y = 20;
+y -= 15;
+console.log(y);
+
+let z = 5;
+z *= 5;
+console.log(z);
+
+let w = 15;
+w /= 2;
+console.log(w);
